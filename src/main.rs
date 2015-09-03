155ef03a97e9512978507fc3bc33e18f2b08f5cc
Vijay Panghal <vpanghal@maginatics.com>
2015-09-03 09:45:40 -0700

    Compiled it with rust nightly.
    rustc 1.4.0-nightly (d50352419 2015-08-29)

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ extern crate spell_corrector;
 
 #[cfg(not(test))]
 fn main() {
+    use std::env;
     use spell_corrector::NwordCorrector;
-    use std::os;
     let corrector = NwordCorrector::new();
-    let args = os::args();
+    let args : Vec<String> = env::args().collect();
     println!("{} -> {}", args[1], corrector.correct(args[1].clone()));
 }
