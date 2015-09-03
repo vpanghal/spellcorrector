8c1c993c7bad2d76b0f602f523687e05ff5b6a7b
Vijay Panghal <vpanghal@maginatics.com>
2015-09-03 09:45:40 -0700

    Compiled with rust nightly.
    rustc 1.4.0-nightly (d50352419 2015-08-29)

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,39 @@
-#![feature(phase)]
-#[phase(plugin)]
-extern crate regex_macros;
+#![feature(plugin)]
+#![plugin(regex_macros)]
+
 extern crate regex;
-use std::io::File;
-use std::str::StrSlice;
-use std::ascii::AsciiExt;
-use std::collections::hashmap::{HashMap, Occupied, Vacant};
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::collections::HashMap;
+use std::path::Path;
+use std::string::String;
+use regex::Regex;
 
 pub struct NwordCorrector {
     // Dictionary of words
-    nwords : HashMap<String, int>
+    nwords : HashMap<String, isize>
 }
 
 impl NwordCorrector {
     // Create new instance.
     pub fn new() -> NwordCorrector {
-        let mut nwords : HashMap<String, int> = HashMap::new();
+        let mut nwords : HashMap<String, isize> =  HashMap::new();
         
-        let contents = File::open(&Path::new("big.txt"))
-                            .read_to_string().unwrap();
-        let re = regex!(r"[a-z]+");
-        
-        for cap in re.captures_iter(contents.as_slice()
-                                    .to_ascii_lower().as_slice()) {
-            match nwords.entry(cap.at(0).to_string()) {
-                Vacant(entry) => { entry.set(1); },
-                Occupied(mut entry) => { *entry.get_mut() += 1; },
+        let path = Path::new("big.txt");
+        let file = match File::open("big.txt") {
+            Err(why) => panic!("failed to open {}: {}", path.display(), why),
+            Ok(f) => f,
+        };
+       
+        let re = Regex::new(r"\w+").unwrap();
+        for line in BufReader::new(file).lines() {
+            for cap in re.captures_iter(&line.unwrap()) {
+                let counter = nwords.entry(String::from(cap.at(0).unwrap())).or_insert(0);
+                *counter += 1;
             }
- 
-        } 
+        }
+
         NwordCorrector { nwords : nwords}
     }
 
@@ -39,67 +44,79 @@ impl NwordCorrector {
             return word
         }
 
-        let mut candidates : HashMap<int, String> = HashMap::new();
-        
-        // Lets do first corrections
+        // Find corrections with distance of 1
         let mut known_edit1 = Vec::new();
-        NwordCorrector::edits(word.as_slice(), & mut known_edit1);
-        for ke1 in known_edit1.iter() {
+        NwordCorrector::edits(&word, &mut known_edit1);
+        let mut word_count = 0;
+        let mut correction : Option<&String> = None;
+        for ke1 in &known_edit1 {
             if self.nwords.contains_key(ke1) {
-                candidates.insert(self.nwords[*ke1], ke1.clone());
-            }
-        }
-
-        if !candidates.is_empty() {
-            return candidates[candidates.keys()
-                .fold(0i, |max, x| { if *x > max { *x } else { max }})].clone()
-        }
-
-        // Lets do second corrections
-        for ke1 in known_edit1.iter() {
-            let mut known_edit2 = Vec::new();
-            NwordCorrector::edits(ke1.as_slice(), & mut known_edit2);
-            for ke2 in known_edit2.into_iter() {
-                if self.nwords.contains_key(&ke2) {
-                    candidates.insert(self.nwords[ke2], ke2);
+                let ke1_count = self.nwords[ke1];
+                if ke1_count > word_count {
+                    word_count = ke1_count;
+                    correction = Some(&ke1);
                 }
             }
         }
-        if !candidates.is_empty() {
-            return candidates[candidates.keys()
-                .fold(0i, |max, x| { if *x > max { *x } else { max }})].clone()
+
+        match correction {
+            Some(word) => { return (*word).clone() }
+            None    => {}
+        }
+
+        // Find corrections with distance of 2
+        let mut correction2 : Option<String> = None;
+        for ke1 in known_edit1.iter() {
+            let mut known_edit2 = Vec::new();
+            NwordCorrector::edits(&ke1, &mut known_edit2);
+            for ke2 in &known_edit2 {
+                if self.nwords.contains_key(ke2) {
+                    let ke2_count = self.nwords[ke2];
+                    if ke2_count >  word_count{
+                        word_count = ke2_count;
+                        // Drop existing string
+                        drop(correction2);
+                        correction2 = Some(String::from(ke2.clone()));
+                    }
+                }
+            }
+        }
+
+        match correction2 {
+            Some(word) => { return word }
+            None    => {}
         }
 
         // No corrections found
         word
     }
 
-
-    fn edits (word : &str, edits :& mut Vec<String>) {
+    fn edits (word : &String, edits :& mut Vec<String>) {
         // Delete
-        for i in range(0, word.len()) {
-            edits.push(format!("{}{}", word.slice_to(i), word.slice_from(i+1)));
+        let len = word.len();
+        for i in 0..len {
+            edits.push(format!("{}{}", &word[0..i], &word[i+1..]));
         }
 
         // Transpose
-        for i in range(0, word.len() - 1) {
-            edits.push(format!("{}{}{}{}", word.slice_to(i), 
-                               word.slice(i+1, i+2), 
-                               word.slice(i, i+1), word.slice_from(i+2)));
+        for i in 0..(len - 1) {
+            edits.push(format!("{}{}{}{}", &word[0..i], 
+                               &word[i+1..i+2], 
+                               &word[i..i+1], &word[i+2..]));
         }
 
         // Replaces
         let alphabet = "abcdefghijklmnopqrstuvwxyz";
-        for i in range(0, word.len()) {
-            for c in alphabet.chars() {
-                edits.push(format!("{}{}{}", word.slice_to(i), c, word.slice_from(i+1)));
-            }
+        for i in 0..len {
+           for c in alphabet.chars() {
+                edits.push(format!("{}{}{}", &word[0..i], c, &word[i+1..]));
+           }
         }
 
         // Insert
-        for i in range(0, word.len()) {
+        for i in 0..len {
             for c in alphabet.chars() {
-                edits.push(format!("{}{}{}", word.slice_to(i), c, word.slice_from(i)));
+                edits.push(format!("{}{}{}", &word[0..i], c, &word[i..]));
             }
         }
     }
